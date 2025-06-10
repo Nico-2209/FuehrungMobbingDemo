2ec4a22956ba5c08157f008fa321379cbc103858
Nico-2209 <121798231+Nico-2209@users.noreply.github.com>
2025-06-10 21:13:45 +0200

    2.1

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-# app.py
-import streamlit as st
 from slider_app import run_slider
 from demos_app import run_demos
+import streamlit as st
 
 st.set_page_config(page_title="Mobbing-Tools", layout="wide")
 st.title("📱 Interaktive Mobbing-Tools")
-
-mode = st.radio("Modul wählen", ["GrenzCheck","App-Demos"], horizontal=True)
-if mode=="GrenzCheck":
+choice = st.radio("Modul wählen", ["GrenzCheck","App-Demos"], horizontal=True)
+if choice=="GrenzCheck":
     run_slider()
 else:
     run_demos()
